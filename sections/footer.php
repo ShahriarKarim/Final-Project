247cadfc2c9a7885b614f9e6c7631a386c2b7da8
PurthaShaariyaar <92152517+PurthaShaariyaar@users.noreply.github.com>
2021-12-05 22:14:53 -0500

    Add files via upload

---
+++ sections/footer.php
@@ -0,0 +1,21 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">OTU Cinema</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2021 OTU Cinema</p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
---
+++ sections/header.php
@@ -0,0 +1,13 @@
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">OTU Cinema</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="index.php">Homepage</a></li>
+            <li><a href="contact-us.php">Contact Us</a></li>
+        </ul>
+    </nav>
+</div>
